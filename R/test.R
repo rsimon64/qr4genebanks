254f0ed97bb3c3885a7bf3e78f43736d256f4030
c5sire <rsimon64@gmail.com>
2018-04-26 10:21:28 -0500

    u

--- R/test.R
+++ R/test.R
@@ -18,6 +18,36 @@ par(mfrow = c(2, 2))
 empty_plot()
 img <- imager::load.image("inia.png")
 rasterImage(img, 0, 5,11, 10)
+text(x = 5.5, y = 5.5, label = Sys.Date(), family = family)
+text(x = 5.5, y = 4.5, label = "Recursos Genéticos", family = family)
+text(x = 5.5, y = 3.5, label = "Banco in-vitro", family = family)
+text(x = 5.5, y = 2.5, label = "PER1234567890", family = family)
+
+image(qrencoder::qrencode_raster("Quagga means zebra"), asp=1, col=c("white", "black"),
+      axes=FALSE, xlab="", ylab="")
+empty_plot()
+text(x = 3, y = 9, label = "Hi you", family = family)
+image(qrencoder::qrencode_raster("This is a test"), asp=1, col=c("white", "black"),
+      axes=FALSE, xlab="", ylab="")
+
+#par(mar=old_mar)
+
+#dev.off()
+
+
+#######################################
+
+
+#pdf(file = "tst.pdf", width = 1.89, height = 2.75, pointsize = 8 )
+#png(file = "tst.png", width = 47 * 8, height = 68 * 8, units = "px")
+
+# old_mar <- par()$mar
+# par(mar=c(0.2, 0.2, 0.2, 0.2))
+# par(mfrow = c(2, 2))
+
+empty_plot()
+img <- imager::load.image("inia.png")
+rasterImage(img, 0, 5,11, 10)
 text(x = 5.5, y = 5, label = Sys.Date(), family = family)
 text(x = 5.5, y = 4, label = "Recursos Genéticos", family = family)
 text(x = 5.5, y = 3, label = "Banco in-vitro", family = family)
@@ -32,6 +62,4 @@ image(qrencoder::qrencode_raster("This is a test"), asp=1, col=c("white", "black
 par(mar=old_mar)
 
 dev.off()
-
-
 
